6fe0920c100c252fd53dbe8f15d92407cf89623f
lee.gitonga <lee.gitonga@turnkeyafrica.com>
2025-08-04 15:10:56 +0300

    index_routes

--- routes/index_routes.js
+++ routes/index_routes.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import userRoutes from '../modules/user/user_routes.js';
 import orderRoutes from '../modules/order/order_routes.js';
+import deliveryRoutes from '../modules/delivery/delivery_routes.js';
 // import orderRoutes, deliveryRoutes
 
 const router = express.Router();
 
 router.use('/logistics/api/v1/user', userRoutes);
 router.use('/logistics/api/v1/order', orderRoutes);
-// router.use('/api/admin', adminRoutes);
+router.use('/logistics/api/v1/delivery', deliveryRoutes)
+// router.use('/api/admin', adminRoutes);s
 
 export default router;
